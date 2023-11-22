b79a673e529d7938616af8eb43c012079baf36e7
samuel silva <prog.samdev@gmail.com>
2023-11-22 16:13:05 -0300

    Main - adjust v3

--- main.go
+++ main.go
@@ -1,14 +1,4 @@
 package main
 
-import (
-	"codewars/digitalroot"
-	"codewars/firstnonrepeating"
-	"fmt"
-)
-
 func main() {
-	dr := digitalroot.DigitalRoot(132189)
-	fmt.Printf("Sum of digits: %d\n", dr)
-	fr := firstnonrepeating.FirstNonRepeating("Go hang a salami, I'm a lasagna hog!")
-	fmt.Printf("First non repeting letter: %s\n", fr)
 }
