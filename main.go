801321ed80fa9428a05116ea7c77a421ce6d1cba
samuel silva <prog.samdev@gmail.com>
2023-11-22 16:43:44 -0300

    removing printf

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"codewars/digitalroot"
-	"codewars/firstnonrepeating"
 	"fmt"
 )
 
 func main() {
 	dr := digitalroot.DigitalRoot(132189)
 	fmt.Printf("Sum of digits: %d\n", dr)
-  fr := = firstnonrepeating.FirstNonRepeating("Go hang a salami, I'm a lasagna hog!")
-  fmt.Printf("First non repeting letter: %c\n", fr)
 }
