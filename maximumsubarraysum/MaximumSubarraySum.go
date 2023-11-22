a99c115d63feeadf3e2abe078c63d66b0a290a62
samuel silva <prog.samdev@gmail.com>
2023-11-22 15:49:27 -0300

    Maximum SubArray Sum

---
+++ maximumsubarraysum/MaximumSubarraySum.go
@@ -0,0 +1,28 @@
+// https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/solutions/go
+
+package maximumsubarraysum
+
+func MaximumSubarraySum(numbers []int) int {
+	var maxSequence int
+	for i := 0; i <= len(numbers); i++ {
+		for j := i; j <= len(numbers); j++ {
+			sequence := numbers[i:j]
+			var sequenceSum int
+			for v := range sequence {
+				sequenceSum += sequence[v]
+			}
+			if maxSequence < sequenceSum {
+				maxSequence = sequenceSum
+			}
+		}
+	}
+	return maxSequence
+}
+
+// The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+// maxSequence [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+// -- should be 6: [4, -1, 2, 1]
+// Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+// Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
