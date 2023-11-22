f41545ba047439a170ef744f04a6fbe33e08b1c1
samuel silva <prog.samdev@gmail.com>
2023-11-22 11:02:56 -0300

    FirstNonRepeating v1

--- firstNonRepeating/FirstNonRepeating.go
+++ firstNonRepeating/FirstNonRepeating.go
@@ -1,5 +1,9 @@
 package firstnonrepeating
 
+import (
+	"strings"
+)
+
 /*
 Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string.
 
@@ -12,7 +16,38 @@ If a string contains all repeating characters, it should return an empty string 
 
 */
 
+// this solution uses map, so I must guarantee the order
+// func FirstNonRepeating(str string) string {
+// 	m := make(map[string]string)
+// 	r := ""
+// 	var keys []string
+// 	for _, k := range str {
+// 		keys = append(keys, strings.ToLower(string((k))))
+// 	}
+
+// 	for _, c := range str {
+// 		k := strings.ToLower(string(c))
+// 		_, seen := m[k]
+// 		if seen {
+// 			m[k] += string(c)
+// 		} else {
+// 			m[k] = string(c)
+// 		}
+// 	}
+// 	for _, v := range keys {
+// 		if len(m[v]) == 1 {
+// 			r = m[v]
+// 			break
+// 		}
+// 	}
+// 	return r
+// }
+
 func FirstNonRepeating(str string) string {
-	//your code here
+	for _, c := range str {
+		if strings.Count(strings.ToLower(str), strings.ToLower(string(c))) < 2 {
+			return string(c)
+		}
+	}
 	return ""
 }
--- main.go
+++ main.go
@@ -1,4 +1,12 @@
 package main
 
+import (
+	"codewars/firstnonrepeating"
+	"fmt"
+)
+
 func main() {
+
+	r := firstnonrepeating.FirstNonRepeating("Go hang a salami, I'm a lasagna hog!")
+	fmt.Print(r)
 }
